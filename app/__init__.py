1e1859e2c93dc7e15f33733e54d674c8c1bfa3b8
Maula Rifqi Setiawan <maularifqi@gmail.com>
2024-12-03 15:14:32 +0700

    Add supplier route endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -26,9 +26,10 @@ def create_app(test_config=None, production_config=os.getenv("PRODUCTION_CONFIG"
     def index():
         return redirect("https://documenter.getpostman.com/view/31842216/2sAYBRGa1z")
     
-    from app.routes import users, raw_materials, seeds
+    from app.routes import users, raw_materials, suppliers, seeds
     app.register_blueprint(users, url_prefix="/api/v1/users")
     app.register_blueprint(raw_materials, url_prefix="/api/v1/raw-materials")
+    app.register_blueprint(suppliers, url_prefix="/api/v1/suppliers")
     app.register_blueprint(seeds, url_prefix="/api/v1/seeds")
     
     return app
